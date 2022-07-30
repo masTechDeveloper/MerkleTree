c14a7c3a1148212ef6a3656a44d3c8d77b089dca
Muhammad Ashir Ansari <ashir342@gmail.com>
2022-07-30 05:48:34 +0500

    setup npm run generate

---
+++ index.js
@@ -0,0 +1,5 @@
+const { generateRootHash } = require("./src/createTree");
+
+(() => {
+  generateRootHash();
+})();
---
+++ src/createTree.js
@@ -0,0 +1,25 @@
+const { MerkleTree } = require("merkletreejs");
+const keccak256 = require("keccak256");
+
+let whitelistAddress = [
+  "0x46c669b59bD12A87A55a91cF7cb5bD87840B3002",
+  "0x97FA19643E35e66ec4cf15c483e3336217A58E12",
+  "0x49f4d5778E7D14A3560407076de4E0dD2ec5DeD4",
+  "0x4635B7475fD34de3D9f54359070a6755Ce0695b7",
+];
+
+async function generateRootHash() {
+  const leafNode = whitelistAddress.map((_add) => keccak256(_add));
+  // console.log("Leaf Node" + leafNode);
+
+  const merkleTree = new MerkleTree(leafNode, keccak256, { sortPairs: true });
+  // console.log(merkleTree);
+
+  // Get Root Hash
+  const rootHash = merkleTree.getRoot();
+  console.log("Root Hash   \n \n" + merkleTree.toString());
+
+  // console.log("Root Hash Buffer = \n" + rootHash);
+}
+
+module.exports = { generateRootHash };
